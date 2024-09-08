d906f8d72f580e08a4807a93e92af3c446c538ee
syedhuzaif199 <syedhuzaif199@gmail.com>
2024-09-08 22:29:40 +0530

    Moved all constants into js/constants.js

---
+++ js/constants.js
@@ -0,0 +1,11 @@
+export const SVG_NAMESPACE = "http://www.w3.org/2000/svg";
+export const CONTROL_POINT_SIZE = 50;
+export const SELECTED_COLOR = "red";
+export const UNSELECTED_COLOR = "black";
+export const MINSCALE = 0.25;
+export const MAXSCALE = 4;
+export const TEXT_SIZE = 30;
+export const TEXTBOX_BASE_WIDTH = 50;
+export const ARROW_HEAD_WIDTH = 7;
+export const ARROW_HEAD_HEIGHT = 7;
+export const TEXT_FONT = "monospace";
