e8f89dfa5fb21cfa46dae4b92b77d822d1feebc5
syedhuzaif199 <syedhuzaif199@gmail.com>
2024-10-08 21:56:07 +0530

    Reworked the entire simulation event handling system to now use a state machine. Added states PLAYING, PAUSED, RESET, and ENDED. Added unique behaviour to each state on each of the different actions (handleNext(), handlePausePlay(), handlePrevious(), handleRewind() and handleFastForward()) Moved most of the code that was common between machines to BasicSimulator, like the retrieveMachine() method Added the onMovedFromResetState() method specifically to be used by Turing machines to retrieve input from the tape and reset some internal states only when the event-state machine moves out of the RESET state The next() method in child classes of BasicSimulator now returns a Promise, with the resolve function returning "true" if the simulation has ended and needs to move to the ENDED state, and "false" if the simulation has not ended yet and is to remain in the current state. Transitioning between states is explicity done by the switch-case block in the handler of each of the above mentioned action, and very rarely a method outside of that changes the states or is supposed to in the future.

--- js/basicSimulator.js
+++ js/basicSimulator.js
@@ -1,32 +1,33 @@
 import { lettersFromRange } from "./utils.js";
 
 export default class BasicSimulator {
-  constructor(svgHandler, tape, onPauseCallback = () => {}) {
+  constructor(svgHandler, tape, onNotPlayingCallback = () => {}) {
     this.svgHandler = svgHandler;
     this.tape = tape;
+    this.tape.blank = "";
+    this.tape.fillTapeWithBlanks();
+    this.tape.moveTapeToStart();
     this.alphabetTextField = document.querySelector("#alphabet-text");
-    this.inputIndex = 0;
-    this.currentTapeCell = null;
     this.states = [];
     this.initialState = null;
-    this.isPlaying = false;
-    this.hasStarted = false;
-    this.isAnimating = false;
-    this.onPauseCallback = onPauseCallback;
+    this.onNotPlayingCallback = onNotPlayingCallback;
     this.inputNode = null;
+    this.states = [];
+    this.finalStates = [];
+    this.simulationStates = Object.freeze({
+      PLAYING: 0,
+      PAUSED: 1,
+      ENDED: 2,
+      RESET: 3,
+    });
+    this.simulationState = this.simulationStates.RESET;
+    this.controlPoints = this.svgHandler.controlPoints;
+    this.transitions = this.svgHandler.transitions;
+    this.isAnimating = false;
   }
+
   getInput() {
     const input = [];
-    // const tape = this.tape.tape;
-    // for (let i = 0; i < tape.children.length; i++) {
-    //   if (tape.children[i].children[0].value === "") {
-    //     break;
-    //   }
-    //   input.push(tape.children[i].children[0].value);
-    // }
-
-    // console.log("Input: ", input);
-
     for (let i = 0; i < this.tape.cellCount; i++) {
       if (this.tape.cells[i].children[0].value === this.tape.blank) {
         break;
@@ -42,52 +43,57 @@ export default class BasicSimulator {
   }
 
   resetSimulation() {
-    this.stopSimulation();
-    this.inputIndex = 0;
-    this.resetTapePosition();
+    this.simulationState = this.simulationStates.RESET;
+    console.log("Reset simulation");
+    this.resetTape();
+    this.svgHandler.setFailStates([]);
+    this.svgHandler.setSuccessStates([]);
+    this.svgHandler.highlightControlPoints([this.initialState]);
+    this.svgHandler.unHighlightAllTransitions();
     this.machine.reset();
-    this.svgHandler.unHighlightAllControlPoints();
-    this.highlightCurrentStates();
+
     this.svgHandler.isEditingDisabled = false;
-    console.error("who called me?");
-    // this.svgHandler.unHighlightAllTransitions();
-    // this.svgHandler.highlightControlPoints([this.initialState]);
+  }
+  playSimulation() {
+    this.simulationState = this.simulationStates.PLAYING;
+    this.svgHandler.isEditingDisabled = true;
+    console.log("Playing simulation");
+    this.loopSimulation();
   }
 
-  /* the commented code below is from the original implementation
-
-    stopSimulation() {
-    this.isPlaying = false;
-    this.isAnimating = false;
-    this.svgHandler.isEditingDisabled = false;
-    this.onPauseCallback();
+  loopSimulation() {
+    console.log("Looping");
+    if (this.simulationState === this.simulationStates.PLAYING) {
+      this.next().then((ended) => {
+        if (!ended) {
+          setTimeout(() => {
+            if (this.simulationState === this.simulationStates.PLAYING) {
+              this.loopSimulation();
+            }
+          }, this.getAnimDelay());
+        } else {
+          this.endSimulation();
+          this.onNotPlayingCallback();
+        }
+      });
+    }
   }
 
-  */
+  pauseSimulation() {
+    this.simulationState = this.simulationStates.PAUSED;
+    console.log("Paused simulation");
+  }
 
-  pauseSimulation() {}
+  endSimulation() {
+    this.simulationState = this.simulationStates.ENDED;
+    this.svgHandler.isEditingDisabled = false;
+    console.log("Ended simulation");
+    this.checkSuccess();
+  }
 
-  resetTapePosition() {
+  // override for Turing machines
+  resetTape() {
     this.tape.moveTapeToStart();
-  }
-
-  handlePlayPause() {
-    if (this.isPlaying) {
-      this.pauseSimulation();
-    } else if (this.hasStarted) {
-      this.resumeSimulation();
-    } else {
-      this.startSimulation();
-    }
-    if (this.isPlaying) {
-      this.svgHandler.isEditingDisabled = true;
-      if (this.inputIndex >= this.getInput().length) {
-        this.resetSimulation();
-      }
-      this.next();
-    } else {
-      this.svgHandler.isEditingDisabled = false;
-    }
   }
 
   getAlphabet() {
@@ -107,63 +113,218 @@ export default class BasicSimulator {
     return resolvedAlphabet;
   }
 
-  handlePrevious() {
-    if (this.inputIndex > 0) {
-      this.inputIndex--;
-    } else {
-      return;
+  handlePlayPause() {
+    switch (this.simulationState) {
+      case this.simulationStates.PLAYING:
+        this.pauseSimulation();
+        this.onNotPlayingCallback();
+        break;
+      case this.simulationStates.RESET:
+        this.onMoveOutOfResetState();
+        if (!this.retrieveMachine()) {
+          break;
+        }
+      case this.simulationStates.PAUSED:
+        this.playSimulation();
+        break;
+      case this.simulationStates.ENDED:
+        // should move to playing state but has to go through reset first
+        // this will possibly have to change in the future as resetSimulation can be thought of as exclusively for moving into the reset state
+        this.resetSimulation();
+        if (!this.retrieveMachine()) {
+          break;
+        }
+        this.playSimulation();
+        break;
     }
-    console.log("previous");
-    this.retrieveMachine();
-    const input = this.getInput();
+  }
 
-    this.machine.run(input.slice(0, this.inputIndex));
-
-    this.tape.moveTapeHead(-1);
-    this.highlightCurrentStates();
-    this.checkSuccess();
+  handlePrevious() {
+    console.log("Simulation state:", this.simulationState);
+    switch (this.simulationState) {
+      case this.simulationStates.ENDED:
+        if (!this.retrieveMachine()) {
+          break;
+        }
+        this.pauseSimulation();
+      case this.simulationStates.PAUSED:
+        if (!this.retrieveMachine()) {
+          break;
+        }
+        this.previous();
+        break;
+      case this.simulationStates.PLAYING:
+        break;
+      case this.simulationStates.RESET:
+        break;
+    }
   }
 
   handleNext() {
-    if (this.isPlaying || this.isAnimating) {
-      return;
-    } else if (this.hasEnded) {
-      this.resetSimulation();
-      return;
-    }
-    console.log("next");
+    switch (this.simulationState) {
+      case this.simulationStates.PLAYING:
+        break;
+      case this.simulationStates.ENDED:
+        break;
 
-    this.next();
+      case this.simulationStates.RESET:
+        this.onMoveOutOfResetState();
+        if (!this.retrieveMachine()) {
+          break;
+        }
+        if (!this.isAnimating) {
+          this.isAnimating = true;
+          this.next().then((ended) => {
+            this.isAnimating = false;
+            if (ended) {
+              this.endSimulation();
+            } else {
+              this.pauseSimulation();
+            }
+          });
+        }
+        break;
+      case this.simulationStates.PAUSED:
+        if (!this.retrieveMachine()) {
+          break;
+        }
+        if (!this.isAnimating) {
+          this.isAnimating = true;
+          this.next().then((ended) => {
+            this.isAnimating = false;
+            if (ended) {
+              this.endSimulation();
+            }
+          });
+        }
+        break;
+    }
   }
 
   handleFastForward() {
-    if (this.isPlaying || this.isAnimating) {
-      return;
+    if (this.simulationState === this.simulationStates.RESET) {
+      this.onMoveOutOfResetState();
     }
-    console.log("fast-forward");
-    this.retrieveMachine();
-    const input = this.getInput();
-    this.tape.moveTapeHead(input.length - this.inputIndex);
-    this.inputIndex = 0;
-    this.machine.run(input);
-    this.highlightCurrentStates();
-    this.inputIndex = input.length;
-    this.checkSuccess();
+    switch (this.simulationState) {
+      case this.simulationStates.PLAYING:
+        this.onNotPlayingCallback();
+      case this.simulationStates.RESET:
+      case this.simulationStates.PAUSED:
+        if (!this.retrieveMachine()) {
+          break;
+        }
+        this.runMachine();
+        this.endSimulation();
+        break;
+      case this.simulationStates.ENDED:
+        break;
+    }
   }
 
   handleRewind() {
-    console.log("rewind");
-    this.resetSimulation();
-    this.isPlaying = false;
-    this.onPauseCallback();
+    switch (this.simulationState) {
+      case this.simulationStates.PLAYING:
+        this.onNotPlayingCallback();
+      case this.simulationStates.PAUSED:
+      case this.simulationStates.ENDED:
+        this.resetSimulation();
+        break;
+      case this.simulationStates.RESET:
+        break;
+    }
   }
 
-  highlightCurrentStates() {
-    const currentStates = this.states.filter((state, i) =>
-      this.machine.currentStates.includes(i)
-    );
-    if (currentStates) {
-      this.svgHandler.highlightControlPoints(currentStates);
+  checkSymbolsInAlphabet() {
+    const input = this.getInput();
+    for (let symbol of input) {
+      if (!this.machine.alphabet.includes(symbol) && symbol !== "") {
+        alert(`Input symbol ${symbol} does not belong to the alphabet`);
+        return false;
+      }
+    }
+  }
+
+  retrieveMachine() {
+    this.transitions = this.svgHandler.transitions;
+    this.controlPoints = this.svgHandler.controlPoints;
+    this.machine.transitions = {};
+
+    const numStates = this.controlPoints.length - 1;
+    this.machine.numStates = numStates;
+    const alphabet = this.getAlphabet();
+    this.machine.alphabet = alphabet;
+
+    this.checkSymbolsInAlphabet();
+
+    this.states = [];
+    this.finalStates = [];
+    const inputNode = this.svgHandler.inputNode;
+    this.initialState = null;
+
+    // find the initial state
+    this.transitions.forEach((transition) => {
+      if (transition.startControlPoint === inputNode) {
+        this.initialState = transition.endControlPoint;
+      }
+    });
+    if (this.initialState === null) {
+      console.error("Initial state not found");
+      return false;
+    }
+
+    // add the initial state
+    this.states[0] = this.initialState;
+    if (this.initialState.isFinal()) {
+      this.finalStates.push(0);
+    }
+    let stateIndex = 1;
+
+    // add the rest of the states
+    this.controlPoints.forEach((controlPoint) => {
+      if (controlPoint !== this.initialState && controlPoint !== inputNode) {
+        this.states[stateIndex] = controlPoint;
+        if (controlPoint.isFinal()) {
+          this.finalStates.push(stateIndex);
+        }
+        stateIndex++;
+      }
+    });
+  }
+
+  // only Turing machines override to provide an implementations
+  onMoveOutOfResetState() {}
+
+  // override for Turing machines
+  runMachine() {
+    this.svgHandler.highlightControlPoints([]);
+    this.svgHandler.unHighlightAllTransitions();
+    const input = [];
+    const startIdx = this.tape.headIndex;
+    for (let i = startIdx; i < this.tape.cellCount; i++) {
+      if (this.tape.getInputFieldAtIndex(i).value === this.tape.blank) {
+        break;
+      }
+      input.push(this.tape.getInputFieldAtIndex(i).value);
+    }
+    console.error("Input:", input);
+    this.machine.run(input);
+    this.tape.moveTapeHead(input.length);
+    this.endSimulation();
+  }
+
+  //override for Turing machines
+  previous() {
+    if (this.tape.headIndex > 0) {
+      this.machine.reset();
+      this.tape.moveTapeHead(-1);
+      const input = [];
+      for (let i = 0; i < this.tape.headIndex; i++) {
+        input.push(this.tape.getInputFieldAtIndex(i).value);
+      }
+      console.log("Input:", input);
+      this.machine.run(input);
+      this.highlightCurrentStates();
+      this.svgHandler.unHighlightAllTransitions();
     }
   }
 }
