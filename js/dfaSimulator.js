f11911964e0554b13fbc43e5f6cf79b0018b22a1
syedhuzaif199 <syedhuzaif199@gmail.com>
2024-10-08 22:36:20 +0530

    Moved code in retrieveMachine() that was common to all machines to method retrieveMachine() in js/basicSimulator.js Removed calls to checkSuccess() and resetSimulation() in the method next() Changed method next() to now returns promises to its callers in basicSimulator, which are responsible for changing the state of the simulation based on the promise returned

--- js/dfaSimulator.js
+++ js/dfaSimulator.js
@@ -3,66 +3,21 @@ import { DANGER_COLOR } from "./constants.js";
 import DFA from "./dfa.js";
 
 export default class DFASimulator extends BasicSimulator {
-  constructor(svgHandler, tape, onPauseCallback = () => {}) {
-    super(svgHandler, tape, onPauseCallback);
+  constructor(svgHandler, tape, onNotPlayingCallback = () => {}) {
+    super(svgHandler, tape, onNotPlayingCallback);
     this.machine = new DFA();
   }
   retrieveMachine() {
-    this.machine.transitions = {};
-    const controlPoints = this.svgHandler.controlPoints;
-    const transitions = this.svgHandler.transitions;
+    super.retrieveMachine();
 
-    const numStates = controlPoints.length - 1;
-    this.machine.numStates = numStates;
-    const alphabet = this.getAlphabet();
-    this.machine.alphabet = alphabet;
-    const input = this.getInput();
-
-    // check if input symbols are in the alphabet
-    for (let symbol of input) {
-      if (!this.machine.alphabet.includes(symbol) && symbol !== "") {
-        alert(`Input symbol ${symbol} does not belong to the alphabet`);
-        return false;
-      }
-    }
-    const finalStates = [];
-    this.states = [];
     const inputNode = this.svgHandler.inputNode;
-    this.initialState = null;
-
-    // find the initial state
-    transitions.forEach((transition) => {
-      if (transition.startControlPoint === inputNode) {
-        this.initialState = transition.endControlPoint;
-      }
-    });
-    if (this.initialState === null) {
-      return false;
-    }
-
-    // add the initial state
-    this.states[0] = this.initialState;
-    if (this.initialState.isFinal()) {
-      finalStates.push(0);
-    }
-    let stateIndex = 1;
-
-    // add the rest of the states
-    controlPoints.forEach((controlPoint) => {
-      if (controlPoint !== this.initialState && controlPoint !== inputNode) {
-        this.states[stateIndex] = controlPoint;
-        if (controlPoint.isFinal()) {
-          finalStates.push(stateIndex);
-        }
-        stateIndex++;
-      }
-    });
-
+    const alphabet = this.getAlphabet();
     // add transitions
     let symbolsNotInAlpha = new Set();
     let multipleTransitions = new Set();
     let alertMessage = "";
-    transitions.forEach((transition) => {
+
+    this.transitions.forEach((transition) => {
       if (transition.startControlPoint === inputNode) {
         return;
       }
@@ -131,7 +86,7 @@ export default class DFASimulator extends BasicSimulator {
     if (missedSymbols.size > 0) {
       console.log("States:", this.states);
       console.log("States?:", this.machine.numStates);
-      console.log("Well, NumStates?:", numStates);
+      console.log("Well, NumStates?:", this.machine.numStates);
       let missedSymbolsStr = "";
       for (let [state, symbol] of missedSymbols) {
         missedSymbolsStr += `${
@@ -148,61 +103,100 @@ export default class DFASimulator extends BasicSimulator {
       return false;
     }
 
-    this.machine.finalStates = finalStates;
+    this.machine.finalStates = this.finalStates;
     return true;
   }
   next() {
-    const input = this.getInput();
-    if (!this.retrieveMachine()) {
-      console.log("Error in retrieving DFA");
-      this.resetSimulation();
-      return;
+    const nextSymbol = this.tape.getSymbolAtHead();
+    console.log("NextSymbol:", nextSymbol);
+
+    if (nextSymbol === this.tape.blank) {
+      return Promise.resolve(true);
     }
-    if (this.inputIndex >= input.length) {
-      console.log("No more input");
-      this.checkSuccess();
-      this.stopSimulation();
-      return;
-    }
-    console.log("Input:", input);
-    const nextSymbol = input[this.inputIndex];
+
     const nextStateNumber =
       this.machine.transitions[[this.machine.currentState, nextSymbol]];
     const currentState = this.states.find(
       (state, i) => i === this.machine.currentState
     );
     const nextState = this.states.find((state, i) => i === nextStateNumber);
+
+    if (nextStateNumber === undefined || !currentState || !nextState) {
+      console.error("Transition or state not found");
+      return Promise.reject();
+    }
+
     console.log("CurrentStateNumber:", this.machine.currentState);
     console.log("NextStateNumber:", nextStateNumber);
+
     const transition = this.svgHandler.transitions.find(
       (transition) =>
         transition.startControlPoint === currentState &&
         transition.endControlPoint === nextState
     );
-    if (transition === undefined) {
-      console.log("No transition found");
-      this.resetSimulation();
-      return;
+
+    if (!transition) {
+      console.error("Transition not found");
+      return Promise.reject();
     }
 
-    this.svgHandler.highlightControlPoints([]);
     this.svgHandler.highlightTransition(transition);
-    this.isAnimating = true;
-    setTimeout(() => {
-      this.svgHandler.unHighlightTransition(transition);
-      this.machine.next(nextSymbol);
-      this.inputIndex++;
-      this.tape.moveTapeHead(1);
-      this.highlightCurrentStates();
-      this.checkSuccess();
-      this.isAnimating = false;
+
+    return new Promise((resolve) => {
       setTimeout(() => {
-        if (this.isPlaying) {
-          this.next();
+        this.svgHandler.unHighlightTransition(transition);
+        this.machine.next(nextSymbol);
+        this.tape.moveTapeHead(1);
+        this.highlightCurrentStates();
+
+        if (this.tape.getSymbolAtHead() === this.tape.blank) {
+          resolve(true);
+        } else {
+          resolve(false);
         }
       }, this.getAnimDelay());
-    }, this.getAnimDelay());
+    });
   }
+
+  // next() {
+  //   const nextSymbol = this.tape.getSymbolAtHead();
+  //   console.log("NextSymbol:", nextSymbol);
+  //   if (nextSymbol === this.tape.blank) {
+  //     this.checkSuccess();
+  //     this.endSimulation();
+  //   }
+  //   const nextStateNumber =
+  //     this.machine.transitions[[this.machine.currentState, nextSymbol]];
+  //   const currentState = this.states.find(
+  //     (state, i) => i === this.machine.currentState
+  //   );
+  //   const nextState = this.states.find((state, i) => i === nextStateNumber);
+  //   console.log("CurrentStateNumber:", this.machine.currentState);
+  //   console.log("NextStateNumber:", nextStateNumber);
+  //   const transition = this.svgHandler.transitions.find(
+  //     (transition) =>
+  //       transition.startControlPoint === currentState &&
+  //       transition.endControlPoint === nextState
+  //   );
+
+  //   this.svgHandler.highlightTransition(transition);
+  //   setTimeout(() => {
+  //     this.svgHandler.unHighlightTransition(transition);
+  //     this.machine.next(nextSymbol);
+  //     this.tape.moveTapeHead(1);
+  //     this.highlightCurrentStates();
+  //     if (this.tape.getSymbolAtHead() === this.tape.blank) {
+  //       this.checkSuccess();
+  //       this.endSimulation();
+  //     } else if (this.simulationState === this.simulationStates.PLAYING) {
+  //       setTimeout(() => {
+  //         if (this.simulationState === this.simulationStates.PLAYING) {
+  //           this.next();
+  //         }
+  //       }, this.getAnimDelay());
+  //     }
+  //   }, this.getAnimDelay());
+  // }
 
   drawMinimizedDFA() {
     if (!this.retrieveMachine()) {
@@ -223,16 +217,13 @@ export default class DFASimulator extends BasicSimulator {
   }
 
   checkSuccess() {
-    const input = this.getInput();
-    if (this.inputIndex >= input.length) {
-      const currentState = this.states.find(
-        (state, i) => i === this.machine.currentState
-      );
-      if (currentState.isFinal()) {
-        this.svgHandler.setSuccessStates([currentState]);
-      } else {
-        this.svgHandler.setFailStates([currentState]);
-      }
+    const currentState = this.states.find(
+      (state, i) => i === this.machine.currentState
+    );
+    if (currentState.isFinal()) {
+      this.svgHandler.setSuccessStates([currentState]);
+    } else {
+      this.svgHandler.setFailStates([currentState]);
     }
   }
 }
